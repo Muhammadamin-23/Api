d7d16cd7bfc1c822b093dfbc35a2c63ba71c5b8d
Muhammadamin_23_05 <138245761+Muhammadamin-23@users.noreply.github.com>
2024-05-07 18:15:27 +0500

    7-may 18:15

--- config/settings.py
+++ config/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%gql31lp@xd26$%kcu(#jc#*tn1g+9fmivi9=&*)wp%3rtz^!1'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(&a9g)l$6c4s@ea(hcubipuww3n#be%uawi$k^h_-pkg_mtj#w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = []
+HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = HOSTS.split(' ') if HOSTS else []
 
 # Application definition
 
