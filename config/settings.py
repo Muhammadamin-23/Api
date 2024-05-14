d26ab53ab5b97e0617de958bd5d2f1732e1e94ab
Muhammadamin_23_05 <138245761+Muhammadamin-23@users.noreply.github.com>
2024-05-14 17:22:59 +0500

    14-may 17:22

--- config/settings.py
+++ config/settings.py
@@ -65,8 +65,11 @@ ROOT_URLCONF = 'config.urls'
 
 CORS_ALLOWED_ORIGINS = (
     "http://localhost:3000",
-    "http://localhost:8000"
+    "http://localhost:8000",
+    "http://localhost:5500",
 )
+
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://127.0.0.1:5500"]
 
 TEMPLATES = [
     {
